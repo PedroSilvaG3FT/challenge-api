4cf4ca1cee2cf4c8f272cbe18e008c4ac83351b8
Pedro Silva <pedro.silva@cbyk.com.br>
2022-03-05 09:42:54 -0300

    Atualizado conexão com banco de dados

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,10 +1,10 @@
-import knex from 'knex';
-import { CONNECTION_DB, CONNECTION_DB_DEV } from '../config/connection-db';
+import knex from "knex";
+import { CONNECTION_DB, CONNECTION_DB_DEV } from "../config/connection-db";
 
 const CONFIG_DB = process.env.IS_PRODUCTION ? CONNECTION_DB : CONNECTION_DB_DEV;
 
 const connection = knex({
-    ...CONFIG_DB
+  ...CONNECTION_DB_DEV,
 });
 
 export default connection;
