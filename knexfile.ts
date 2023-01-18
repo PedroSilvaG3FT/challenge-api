46f172df4418b112e91ab627d8c8f6b3cd8fde53
Pedro Silva <pedro.silva@cbyk.com.br>
2023-01-18 01:26:27 -0300

    fix: database

--- knexfile.ts
+++ knexfile.ts
@@ -4,7 +4,7 @@ import { CONNECTION_DB, CONNECTION_DB_DEV } from "./src/config/connection-db";
 // const CONFIG_DB = process.env.IS_PRODUCTION ? CONNECTION_DB : CONNECTION_DB_DEV;
 
 module.exports = {
-  ...CONNECTION_DB_DEV,
+  ...CONNECTION_DB,
 
   migrations: {
     directory: path.resolve(__dirname, "src", "database", "migrations"),
